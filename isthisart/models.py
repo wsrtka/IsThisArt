441bcfde0d339b7a8ed15c38399c35f1b255216e
wsrtka <witold.serwatka@pm.me>
2023-02-27 12:37:51 +0100

    add model class

--- isthisart/models.py
+++ isthisart/models.py
@@ -1,1 +1,12 @@
 """Module containing various deep learning models."""
+
+import torch
+
+from torch import nn
+
+
+class TinyVGG(nn.Module):
+    """
+    TinyVGG model based on:
+    https://poloclub.github.io/cnn-explainer/
+    """
